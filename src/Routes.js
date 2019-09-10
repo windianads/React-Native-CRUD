8bf728659f67e3d801e94fa11e5cdae10dd9fac8
windianads <dinda.windiana.sekaryus@gmail.com>
2019-09-10 09:52:14 +0700

    React Native CRUD

---
+++ src/Routes.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+import {Router, Stack, Scene} from 'react-native-router-flux';
+
+
+import Login from './pages/Login';
+
+import Register from './pages/Register';
+
+
+
+export default class Routes extends Component<{}> {
+
+render() {
+
+return(
+
+<Router>
+
+<Stack key="root" hideNavBar={true}>
+
+<Scene key="login" component={Login} title="Login" initial={true}/>
+
+<Scene key="register" component={Register} title="Register"/>
+
+</Stack>
+
+</Router>
+
+)
+
+}
+
+}
